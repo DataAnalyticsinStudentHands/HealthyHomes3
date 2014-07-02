9947c259802de8ea81ed7ca7509269c2a7c21da2
Gaby Cruz <gcruz.a10@gmail.com>
2014-07-02 15:12:57 -0500

    updated files in www folder.  also installed a bower package (angular-route) to make it work

--- www/js/app.js
+++ www/js/app.js
@@ -3,135 +3,41 @@
 /* App Module */
 
 var HHApp = angular.module('HHApp', [
-    'ui.router','HHControllers',
-  //'ui.router', 'ngAnimate', 'ngTouch', 'ngGesture','ngResource','databaseServicesModule','HealthyHomesControllers'
+  'ngRoute', 'HHControllers', 'ui.router', 'ngAnimate'
 ]); //dependencies
 
 HHApp.config(
-  function($stateProvider) {
+  function($stateProvider, $urlRouterProvider) {
     $stateProvider.
-    state('logo', {
+      state('logo', {
           url: "",
           views: {
             "app": { templateUrl: "partials/logoPage.html"}
           }
-      }).
+      }).    
       state('login', {
           url: "/login",
           views: {
-            "app": {templateUrl: "partials/loginPage.html"}
+            "app": {templateUrl: "partials/loginPage.html"} 
           }
       }).
       state('layout', {
           url: "/login/layout",
           views: {
-            "app": {templateUrl: "partials/layout.html"}
+            "app": {templateUrl: "partials/layoutPage.html"} 
           }
       }).
       state('questions', {
           url: "/login/questions",
           views: {
-            "app": {templateUrl: "partials/questions.html"}
+            "app": {templateUrl: "partials/questions.html"} 
           }
-      }).
+      }).    
       state('camera', {
           url: "/login/camera",
           views: {
-            "app": {templateUrl: "partials/camera.html"}
+            "app": {templateUrl: "partials/camera.html"} 
           }
       });
   });
-    
-//        .state('home', {
-//            url: "/home",
-//            templateUrl: "/partials/home.html"
-////          views: {
-////            "app":      { templateUrl: "/partials/home.html"},
-////            "sideselectbar": { templateUrl: "partials/sideselectbar.html"}
-////            //"menuBar":  { templateUrl: "partials/menuBar.html", controller: 'menuCtrl'}
-////          }
-//        })
-//        .state('login', {
-//            
-//        })
-//        .state('user', {
-//            
-//        })
-//        .state('layout', {
-//            //should there be nested views for the different floors?
-//        })
-//        .state('questions', {
-//            
-//        })
-//        .state('manager', {
-//            
-//        });
-//    });
-        
-        /*state('login', {
-          url: "/phones/:phoneId",
-          views: {
-            "app": {templateUrl: "partials/beatles.html", controller: 'PhoneListCtrl'} 
-          }
-        })*/
-      // Anonymous routes http://www.frederiknakstad.com/2014/02/09/ui-router-in-angular-client-side-auth/
-    //at some point might help with the residential versions - now there is no anonymous use
-   /*     .state('anon', {
-            abstract: true,
-            template: "<ui-view/>",
-            data: {
-                access: access.anon
-            }
-        })
-        .state('anon.login', {
-            url: '/login/',
-            templateUrl: 'login',
-            controller: 'LoginCtrl'
-        })
-        .state('anon.register', {
-            url: '/register/',
-            templateUrl: 'register',
-            controller: 'RegisterCtrl'
-        });*/
-
-// Regular user routes
-/*$stateProvider
-    .state('user', {
-        abstract: true,
-        template: "<ui-view/>",
-        data: {
-            access: access.user
-        }
-    })
-    .state('user.home', {
-        url: '/',
-        templateUrl: 'home'
-    })
-    .state('user.private', {
-        abstract: true,
-        url: '/private/',
-        templateUrl: 'private/layout'
-    })
-    .state('user.private.home', {
-        url: '',
-        templateUrl: 'private/home'
-    })
-    .state('user.private.nested', {
-        url: 'nested/',
-        templateUrl: 'private/nested'
-    })
-    .state('user.private.admin', {
-        url: 'admin/',
-        templateUrl: 'private/nestedAdmin',
-        data: {
-            access: access.admin
-        }
-    });
-
-      state('loginHomePage', {
-          views: {
-            "app": {url: "", templateUrl: "partials/loginPage.html"}
-          }
-      });*/
-  
 
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,79 +2,11 @@
 
 /* Controllers */
 
-var HHControllers = angular.module('HHControllers',['ui.router']);
-
-HHControllers.controller('PhoneListCtrl', ['$scope','$http', //may not need this at all
+var HHControllers = angular.module('HHControllers', ['ui.router']);
+HHControllers.controller('PhoneListCtrl', ['$scope', '$http',
   function($scope, $http) {
     $http.get('phones/phones.json').success(function(data) {
       $scope.phones = data;
     });
-  }])
-.controller('loginCtrl', ['$scope', 'databaseConnection',
-  function($scope, $rootScope, databaseConnection) {
-    $scope.submit = function() {
-        if ($scope.userName && $scope.passWord) {
-            $scope.loginMsg = "Thank you for logging in!";
-            $scope.loginResult = databaseConnection.login({userName:$scope.userName, passWord:$scope.passWord});
-            $scope.userName = '';
-            $scope.passWord = '';
-        } else if (!$scope.userName) {
-            $scope.loginMsg = "Please enter your username!";
-            $scope.loginResult = "";
-        } else if (!$scope.passWord) {
-            $scope.loginMsg = "Please enter your password!";
-            $scope.loginResult = "";
-        }
-    };
-//    $rootScope.$on("$stateChangeStart",  //http://www.frederiknakstad.com/2014/02/09/ui-router-in-angular-client-side-auth/
-//    function (event, toState, toParams, 
-//              fromState, fromParams) {
-//        if (!Auth.authorize(toState.data.access)) {
-//            $rootScope.error = "Access denied";
-//            event.preventDefault();
-//        };
-//        if(fromState.url === '^') {
-//            if(Auth.isLoggedIn()) {
-//                //$state.go('user.home');
-//            } else {
-//                //$rootScope.error = null;
-//                //$state.go('anon.login');
-//            };
-//        };
-//    });
-  }]);                
-
-//.controller('menuCtrl', ['$scope', '$state',
-//  function($scope, $state) {
-//    $scope.goBack = function() {
-//        //window.history.back();
-//        $state.go('^');
-//    };
-//  }]);
-
-/*//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-
-function myCtrl($scope){
-
-// %%%%%%%%%% to talk to DB %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-var databaseController = angular.module('databaseControllerModule', []);
-databaseController.controller('dbDisplayController', ['$scope', 'databaseConnection',
-  function($scope, databaseConnection) {
-    $scope.hello = databaseConnection.hello();
-    $scope.queryResult = databaseConnection.query();    
-    databaseConnection.login({"data": {userName:"user", passWord: "pass"}}, function(value){
-        $scope.login = value;
-        $scope.loginData = JSON.parse(value.data);
-    });
-    
-    $scope.send = function(){
-        $scope.user = databaseConnection.queryWebService({"queryA":$scope.queryA});
-        $scope.pass = databaseConnection.queryWebService({"query1":$scope.query1});        
-        $scope.inputText='';
-        
-    };
-      
-  }]);*/
-
+  }]);
 
