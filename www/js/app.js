f215117982c6d737b445ddae1de9398c95f58927
Gaby Cruz <gcruz.a10@gmail.com>
2014-07-07 11:15:27 -0500

    show/hide for tabs

--- www/js/app.js
+++ www/js/app.js
@@ -27,12 +27,37 @@ HHApp.config(
             "app": {templateUrl: "partials/layoutPage.html"}
           }
       }).
-      state('questions', {
-          url: "/login/questions",
-          views: {
-            "app": {templateUrl: "partials/questions.html"} 
-          }
-      }).    
+	  state('layout.first', {
+		  url: "/login/layout/questions",
+		  controller: 'mainController',
+		  views: {
+			"a": {templateUrl: "partials/questions.html"}
+		  }
+	  }).
+	  state('layout.second', {
+		  url: "/login/layout/questions1",
+		  views: {
+			"b": {templateUrl: "partials/questions1.html"}
+		  }
+	  }).
+	  state('layout.third', {
+		  url: "/login/layout/questions2",
+		  views: {
+			"c": {templateUrl: "partials/questions2.html"}
+		  }
+	  }).
+	  state('layout.fourth', {
+		  url: "/login/layout/questions3",
+		  views: {
+			"d": {templateUrl: "partials/questions3.html"}
+		  }
+	  }).
+	  state('layout.fifth', {
+		  url: "/login/layout/summary",
+		  views: {
+			"e": {templateUrl: "partials/summary.html"}
+		  }
+	  }).
       state('camera', {
           url: "/login/camera",
           views: {
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,15 +2,59 @@
 
 /* Controllers */
 
-var HHControllers = angular.module('HHControllers', ['ui.router', 'ngTouch']);
+var HHControllers = angular.module('HHControllers', ['ui.router']);
+HHControllers
 
-HHControllers.controller('PhoneListCtrl', ['$scope', '$http',
+.controller('PhoneListCtrl', ['$scope', '$http',
   function($scope, $http) {
     $http.get('phones/phones.json').success(function(data) {
       $scope.phones = data;
     });
-  }]);
+  }])
+
+.controller('mainController', function($scope) {		//show & hide
+	$scope.goCats = false;
+});
+
+/*    --------------    ng-repeat     ------------      */
+
+function myCtrl($scope){
+    $scope.icons=[{
+    src: 'img/greenflag.PNG'
+  },{
+    src: 'img/yellowflag.PNG'
+  },{
+    src: 'img/redflag.PNG'
+  }];
+}
+
+function myCtrl2($scope){
+    $scope.icons=[{
+    src: 'img/pencil.PNG'
+  },{
+    src: 'img/ruler.PNG'
+  },{
+    src: 'img/eraser.PNG'
+  }];
+}
+
+function myCtrl3($scope){
+    $scope.icons=[{
+    src: 'img/sink.PNG'
+  },{
+    src: 'img/window.PNG'
+  },{
+    src: 'img/stairs.PNG'
+  },{
+    src: 'img/fire.PNG'
+  },{
+    src: 'img/toilet.PNG'
+  },{
+    src: 'img/tub.PNG'
+  },{
+    src: 'img/door.PNG'
+  }];
+}
 
 
-/* HHControllers.controller('sideTabsCtrl', */
 
