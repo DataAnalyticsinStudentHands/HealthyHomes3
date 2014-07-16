9091960ad22f7ef5f30eaee79c1f0153f7f8ccc8
Gaby Cruz <gcruz.a10@gmail.com>
2014-07-16 16:55:20 -0500

    now for-loop goes through json array of objects after doing GET request (so you can pick out which elements you want to display).  tried doing ng-repeat (for side tabs) but it doesn't work so far bc of seperate ui-sref's.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -53,37 +53,83 @@ HHControllers
   },{
     icontype:  'svg_circle'
   }];
-
+	
+    $scope.sideTabs=[{ //how did you link the images without the .PNG extension?
+    icontype:  'sidetab1'/*, ui-sref: 'questions.first'*/
+  },{
+    icontype:  'sidetab2'/*, state: 'questions.second'*/
+  },{
+    icontype:  'sidetab3'/*, state: 'questions.third'*/
+  },{
+    icontype:  'sidetab4'/*, state: 'questions.fourth'*/
+  },{
+    icontype:  'sidetab5'/*, state: 'questions.fifth'*/
+  },];
 })
 
 .controller('questionsController1', ['$scope', '$http',						
   function($scope, $http) {
+	  $scope.questions=[];
     $http.get('json/asthma.json').success(function(data) {
-      $scope.questions = data;
-    });
-    $scope.orderProp = 'ID';
+		$scope.questions = data;
+		/*$scope.orderProp = 'ID';*/
+		var text = "";
+
+		for (var i = 0; i < $scope.questions.length; i++) {
+			text += $scope.questions[i].questionText + "<br>";
+		}
+		document.getElementById("forloop").innerHTML = text;
+		console.log("asthma array length is "+$scope.questions.length); //just to see
+	}); 
   }])
 
 .controller('questionsController2', ['$scope', '$http',						
   function($scope, $http) {
+	  $scope.questions=[];	  
     $http.get('json/fall_prevention.json').success(function(data) {
       $scope.questions = data;
-    });
-    $scope.orderProp = 'ID';
+    
+    /*$scope.orderProp = 'ID';*/
+	var text = "";
+
+	for (var i = 0; i < $scope.questions.length; i++) {
+		text += $scope.questions[i].questionText + "<br>";
+	}
+	document.getElementById("forloop").innerHTML = text;  
+	console.log("fall prevention array length is "+$scope.questions.length); //just to see		
+	});  
   }])
 
 .controller('questionsController3', ['$scope', '$http',						
   function($scope, $http) {
+	  $scope.questions=[];	  
     $http.get('json/lead.json').success(function(data) {
       $scope.questions = data;
-    });
-    $scope.orderProp = 'ID';
+    
+    /*$scope.orderProp = 'ID';*/
+	var text = "";
+
+	for (var i = 0; i < $scope.questions.length; i++) {
+		text += $scope.questions[i].questionText + "<br>";
+	}
+	document.getElementById("forloop").innerHTML = text;	
+	console.log("lead array length is "+$scope.questions.length); //just to see		
+	});  
   }])
 
 .controller('questionsController4', ['$scope', '$http',						
   function($scope, $http) {
+	  $scope.questions=[];	  
     $http.get('json/pesticide.json').success(function(data) {
       $scope.questions = data;
-    });
-    $scope.orderProp = 'ID';
+    
+    /*$scope.orderProp = 'ID';*/
+	var text = "";
+
+	for (var i = 0; i < $scope.questions.length; i++) {
+		text += $scope.questions[i].questionText + "<br>";
+	}
+	document.getElementById("forloop").innerHTML = text;	
+	console.log("pesticide array length is "+$scope.questions.length); //just to see
+	});  
   }]);	// end of controllers
